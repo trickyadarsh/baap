0255fcb9cce5c1969048fdb430ca538046fc142d
trickyadarsh <121372273+trickyadarsh@users.noreply.github.com>
2023-06-15 05:58:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ async def is_admins(chat_id: int):
         )
     ]
 
-START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/24474d87a093a868e538f.jpg")
+
 
 START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/24474d87a093a868e538f.jpg"
 @bot.on_message(filters.command("start"))
